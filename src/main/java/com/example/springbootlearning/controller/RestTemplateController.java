fae76f95164e9f7c298f0b3afeade03e14e721d8
Yuvaraj Palanisamy <yp00801161@techmahindra.com>
2023-05-09 21:25:42 +0200

    Rest Template - exchange

--- src/main/java/com/example/springbootlearning/controller/RestTemplateController.java
+++ src/main/java/com/example/springbootlearning/controller/RestTemplateController.java
@@ -3,13 +3,17 @@ package com.example.springbootlearning.controller;
 import com.example.springbootlearning.model.Post;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -17,6 +21,8 @@ import java.util.List;
 public class RestTemplateController {
     @Autowired
     private RestTemplate restTemplate;
+
+    private String all_posts_url = "http://jsonplaceholder.typicode.com/posts/1";
 
     @ApiOperation(value = "welcome Api")
     @GetMapping("/welcome")
@@ -45,4 +51,13 @@ public class RestTemplateController {
         var result = restTemplate.getForEntity("http://jsonplaceholder.typicode.com/posts/1", Post.class);
         return new ResponseEntity<Post>(result.getBody(), HttpStatus.OK);
     }
+
+    @ApiOperation(value = "to get one post from http://jsonplaceholder.typicode.com/posts/1")
+    @GetMapping("/postExchange/1")
+    public ResponseEntity<Post> exchangeRestTemplate() throws URISyntaxException {
+        RequestEntity requestEntity = new RequestEntity(HttpMethod.GET,new URI(all_posts_url));
+        var result = restTemplate.exchange(requestEntity,Post.class);
+        return new ResponseEntity<Post>(result.getBody(), HttpStatus.OK);
+    }
+
 }
